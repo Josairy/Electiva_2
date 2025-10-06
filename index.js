9bc350f95409db310044aa980af04e029a649cb7
Josairy <20240008@itla.edu.do>
2025-10-05 23:56:54 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// Programa Hola Mundo
+console.log("¡Hola Mundo!");
+console.log("Bienvenido a GitHub Actions");
+console.log("Estudiante: Josairy");
+console.log("Materia: Electiva 2");
+console.log("Fecha: Octubre 2025");
